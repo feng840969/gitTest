f430ed152f0d85fb7e794e4ee65ddf7027b0480d
STEVE <f_steve@sina.com>
2018-12-27 16:18:50 +0800

    first commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by STEVE on 2018/12/27.
+//  Copyright © 2018年 STEVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ gitTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by STEVE on 2018/12/27.
+//  Copyright © 2018年 STEVE. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
