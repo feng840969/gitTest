a64cad70d27d7597282cd07c4d2d8ad236251b94
STEVE <f_steve@sina.com>
2018-12-28 10:15:15 +0800

    test

---
+++ gitTest/teatTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  teatTableViewCell.h
+//  gitTest
+//
+//  Created by STEVE on 2018/12/28.
+//  Copyright © 2018年 STEVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface teatTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ gitTest/teatTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  teatTableViewCell.m
+//  gitTest
+//
+//  Created by STEVE on 2018/12/28.
+//  Copyright © 2018年 STEVE. All rights reserved.
+//
+
+#import "teatTableViewCell.h"
+
+@implementation teatTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
